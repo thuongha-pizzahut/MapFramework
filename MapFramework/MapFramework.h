11d51084fceef2f1d76891f039184d52318afd12
thuongha-pizzahut <37992342+thuongha-pizzahut@users.noreply.github.com>
2018-11-23 21:39:15 +0700

    Add files via upload

---
+++ MapFramework/MapFramework.h
@@ -0,0 +1,19 @@
+//
+//  MapFramework.h
+//  MapFramework
+//
+//  Created by Xuan Thuong on 11/23/18.
+//  Copyright © 2018 PHDVAsia01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapFramework.
+FOUNDATION_EXPORT double MapFrameworkVersionNumber;
+
+//! Project version string for MapFramework.
+FOUNDATION_EXPORT const unsigned char MapFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapFramework/PublicHeader.h>
+
+
